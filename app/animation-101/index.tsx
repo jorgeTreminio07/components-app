cf755e0b0bdff686826bb742d83e47e3ea17205e
TreminioDev <eduardotreminio10@gmail.com>
2026-03-18 19:44:40 -0600

    botones fade in y fadeout

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native';
+import ThemedButton from "@/presentation/shared/ThemedButton";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { Text } from "react-native";
 
 const Animation101Screen = () => {
   return (
-    <View>
+    <ThemedView>
       <Text>Animation101Screen</Text>
-    </View>
+
+      <ThemedButton className="mb-5" onPress={() => console.log("fadeIn")}>
+        FadeIn
+      </ThemedButton>
+
+      <ThemedButton className="mb-5" onPress={() => console.log("fadeOut")}>
+        FadeOut
+      </ThemedButton>
+    </ThemedView>
   );
 };
 export default Animation101Screen;
