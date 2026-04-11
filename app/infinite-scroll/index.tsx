23ab7ddb1d22894d34d8990af09779bd7f4b5c02
TreminioDev <eduardotreminio10@gmail.com>
2026-04-10 20:49:53 -0600

    infinite scroll

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native';
+import { useThemeColor } from "@/hooks/use-theme-color";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { useState } from "react";
+import { ActivityIndicator, Image, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 const InfiniteScrollScreen = () => {
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+  const primaryColor = useThemeColor({}, "primary");
+
+  const loadMore = () => {
+    const newNArray = Array.from({ length: 5 }, (_, i) => numbers.length + i);
+
+    setTimeout(() => {
+      setNumbers([...numbers, ...newNArray]);
+    }, 3000);
+  };
+
   return (
-    <View>
-      <Text>InfiniteScrollScreen</Text>
-    </View>
+    <ThemedView>
+      <FlatList
+        data={numbers}
+        renderItem={({ item }) => <ListItem number={item} />}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.6}
+        ListFooterComponent={() => (
+          <View style={{ height: 150, justifyContent: "center" }}>
+            <ActivityIndicator size={40} color={primaryColor} />
+          </View>
+        )}
+      />
+    </ThemedView>
   );
 };
 export default InfiniteScrollScreen;
+
+interface ListItemProps {
+  number: number;
+}
+
+const ListItem = ({ number }: ListItemProps) => {
+  return (
+    <Image
+      source={{ uri: `https://picsum.photos/id/${number}/500/400` }}
+      style={{ width: "100%", height: 400 }}
+    />
+  );
+};
